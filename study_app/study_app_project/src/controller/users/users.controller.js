16c3cd940542639468dd532e7a84e1834f826cd6
EliasEcheverria <echeverriaselias81@gmail.com>
2023-05-19 16:21:44 -0400

    backend Update

--- study_app/study_app_project/src/controller/users/users.controller.js
+++ study_app/study_app_project/src/controller/users/users.controller.js
@@ -1,6 +1,7 @@
 const { sequelize } = require("../../connection");
 const { UserModel } = require("../../model/users.model");
 const UserService = require("../../service/users.service");
+const jwt = require('jsonwebtoken');
 /*Se pone llaves solo para recuperar objetos exportados */
 const listar = async function (req, res) {
     console.log("listar usuarios");
@@ -90,6 +91,67 @@ const eliminar = async function (req, res) {
     }
 };
 
+const login = async function(req, res){
+    console.log("login usuarios desde controller");
+    try {
+        const usersDB = await sequelize.query
+        ("SELECT * FROM users WHERE email = '"+req.body.email+"' AND password = '"+req.body.password+"'");
+        console.log("users", usersDB);
+        let user = null;
+        if(usersDB.length > 0 && usersDB[0].length > 0){
+            user = usersDB[0][0];
+            if (user.token){
+                res.json({
+                    success : false,
+                    error : "El usuario ya está autenticado"
+                });
+                return;
+            }
+            let token = jwt.sign({
+                codigo : user.codigo,
+                name : user.name,
+                last_name : user.last_name,
+                avatar : user.avatar,
+                email : user.email 
+            }, 'password');
+            const usersDBUpdate = await sequelize.query("UPDATE users SET token = '"+token+"' WHERE id = "+user.id)
+            res.json({
+                success : true,
+                user
+            });
+        }else{
+            res.json({
+                success : false,
+                error : "Usuario no encontrado"
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success : false,
+            error : error.message
+        });
+    }
+};
+
+const logout = async function(req, res){
+    try {
+        console.log("Token: ", req.headers.authorization);
+        const usersDB = await sequelize.query("SELECT * FROM users WHERE token = '"+req.headers.authorization+"'");
+        console.log("users: ", usersDB);
+        const usersDBUpdate = await sequelize.query("UPDATE users SET token = null WHERE id = "+usersDB[0][0].id+"");
+        res.json({
+            success : true
+        });
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success : false,
+            error : error.message
+        });
+    }
+}
+
 module.exports = {
-    listar, consultarPorCodigo, actualizar, eliminar
+    listar, consultarPorCodigo, actualizar, eliminar, login, logout
 };
--- study_app/study_app_project/src/route/users/users.route.js
+++ study_app/study_app_project/src/route/users/users.route.js
@@ -5,4 +5,6 @@ module.exports = function (app) {
     app.get("/user/:id", userController.consultarPorCodigo);
     app.post("/users/update", userController.actualizar);
     app.delete("/users/delete/:id", userController.eliminar);
+    app.post("/users/login", userController.login);
+    app.post("/users/logout", userController.logout);
 }
